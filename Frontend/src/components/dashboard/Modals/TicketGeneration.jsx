23d29e1f4a2df1bbe0ab74031a80c250e1c3c7c3
tarun142605 <tarun142605@gmail.com>
2024-04-14 12:53:26 +0530

    ticket creation

--- Frontend/src/components/dashboard/Modals/TicketGeneration.jsx
+++ Frontend/src/components/dashboard/Modals/TicketGeneration.jsx
@@ -1,23 +1,42 @@
 import React from "react";
+import { useState } from "react";
 
 function TicketGeneration() {
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    comments: '',
+    images: []
+  });
+
+  const handleChange = (e) => { 
+    const { name, value } = e.target;
+    if (name === 'images') {
+      setFormData({
+        ...formData,
+        [name]: e.target.files
+      });
+    } else {
+      console.log(name, value);
+      setFormData({
+        ...formData,
+        [name]: value
+      });
+    };
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData.entries());
-    console.log(data);
-
-
     let conn = new XMLHttpRequest();
-        conn.open("POST", "http://localhost:3000/collectionAgent/registerCollectionAgent", true);
+        conn.open("POST", "http://localhost:3000/ticket/createTicket", true);
         conn.setRequestHeader("Content-Type", "application/json");
         conn.send(JSON.stringify(formData));
         conn.onreadystatechange = function() {
             console.log(this.status);
           if (this.status === 200) {
-            // let data = JSON.parse(this.responseText);
-            // console.log(data);
+            console.log(this.responseText);
             // toast.remove()
             // toast.success("Registered Successfully");
           }
@@ -33,19 +52,22 @@ function TicketGeneration() {
       <h1>Ticket Generation</h1>
       <form>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" required />
+        <input type="text" id="name" name="name" onChange={handleChange} value={formData.name} required />
         <br />
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" onChange={handleChange} value={formData.email} required />
         <br />
         <label htmlFor="phone">Phone</label>
-        <input type="mobile" id="phone" name="phone" required />
+        <input type="mobile" id="phone" name="phone" onChange={handleChange} value={formData.phone} required />
         <br />
         <label htmlFor="image">Images</label>
-        <input type="file" name="images" id="images" multiple />
+        <input type="file" name="images" onChange={handleChange} id="images" multiple />
         <br />
         <label htmlFor="message">Comments</label>
-        <textarea id="message" name="message" required />
+        <textarea id="comments" name="comments" onChange={handleChange} value={formData.comments} required />
+        <br />
+        <button>Cancel</button>
+        <button onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
